5033dfd7bea34ab4ae9c53416d49e3cabc5ec98f
Engineer <Engineer@DESKTOP-U9D8BHS>
2016-10-02 12:04:03 +0300

    Add project files.

---
+++ lison1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lison1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double number1, number2;
+            Double result;
+            Console.WriteLine("Enter the first number : ");
+            number1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            number2 = Double.Parse(Console.ReadLine());
+            result = number1 * number2;
+            Console.WriteLine("The result is :" + result);
+            Console.ReadKey();
+            // برنامج ضرب عددين 
+        }
+    }
+}
